c5fb5b7b0f6386c97fdd7a701ad9327a52a17a13
ruldiaz <rulhdiaz@gmail.com>
2024-09-15 11:59:06 -0600

    commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,22 +1,19 @@
+import com.aluracursos.screenmtach.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre = "Encanto";
-        miPelicula.fechaDeLanzamiento = 2021;
-        miPelicula.duracionEnMinutos = 120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(7.8);
         miPelicula.evalua(10);
-        System.out.println(miPelicula.sumaDeLasEvaluaciones);
-        System.out.println(miPelicula.totalDeLasEvaluaciones);
+        miPelicula.evalua(8);
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
         System.out.println(miPelicula.calculaMedia());
-
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre = "Matrix";
-        otraPelicula.fechaDeLanzamiento = 1998;
-        otraPelicula.duracionEnMinutos = 180;
-
-        //otraPelicula.muestraFichaTecnica();
+        System.out.println(miPelicula.getNombre());
     }
 }
