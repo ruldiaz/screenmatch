54860413c37aa46001a621d3f8efe3ca10ea510e
ruldiaz <rulhdiaz@gmail.com>
2024-09-15 10:55:32 -0600

    commit

---
+++ src/Pelicula.java
@@ -0,0 +1,23 @@
+public class Pelicula {
+    String nombre;
+    int fechaDeLanzamiento;
+    int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    double sumaDeLasEvaluaciones;
+    int totalDeLasEvaluaciones = 0;
+
+    void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración de la película: " + duracionEnMinutos);
+    }
+
+    void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+}
