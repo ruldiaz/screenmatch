76dedc613c82d4d5c1e6369edc71ccb3b27d2cc3
ruldiaz <rulhdiaz@gmail.com>
2024-09-16 14:40:01 -0600

    commit

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,6 @@
 import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempos;
+import com.aluracursos.screenmatch.calculos.FiltroRecomendacion;
+import com.aluracursos.screenmtach.modelos.Episodio;
 import com.aluracursos.screenmtach.modelos.Pelicula;
 import com.aluracursos.screenmtach.modelos.Serie;
 
@@ -37,5 +39,16 @@ public class Principal {
         calculadora.incluye(casaDragon);
         calculadora.incluye(otraPelicula);
         System.out.println("Tiempo necesario para ver tus titulos favoritos: " + calculadora.getTiempoTotal() + " minutos");
+
+        FiltroRecomendacion filtroRecomendacion = new FiltroRecomendacion();
+        filtroRecomendacion.filtra(miPelicula);
+
+        Episodio episodio = new Episodio();
+        episodio.setNumero(1);
+        episodio.setNombre("La casa Targaryen");
+        episodio.setSerie(casaDragon);
+        episodio.setTotalVisualizaciones(50);
+
+        filtroRecomendacion.filtra(episodio);
     }
 }
--- src/com/aluracursos/screenmtach/modelos/Pelicula.java
+++ src/com/aluracursos/screenmtach/modelos/Pelicula.java
@@ -1,6 +1,8 @@
 package com.aluracursos.screenmtach.modelos;
 
-public class Pelicula extends  Titulo {
+import com.aluracursos.screenmatch.calculos.Clasificacion;
+
+public class Pelicula extends  Titulo implements Clasificacion {
     private String director;
 
     // getters
@@ -11,5 +13,10 @@ public class Pelicula extends  Titulo {
     // setters
     public void setDirector(String director){
         this.director = director;
+    }
+
+    @Override
+    public int getClasificacion() {
+        return (int) (calculaMedia() / 2);
     }
 }
