1d063a78ce54e5e2e0cf372712dc399966636208
ruldiaz <rulhdiaz@gmail.com>
2024-09-16 11:44:36 -0600

    commit

--- src/com/aluracursos/screenmtach/modelos/Pelicula.java
+++ src/com/aluracursos/screenmtach/modelos/Pelicula.java
@@ -1,64 +1,14 @@
 package com.aluracursos.screenmtach.modelos;
 
 public class Pelicula {
-    private String nombre;
-    private int fechaDeLanzamiento;
-    private int duracionEnMinutos;
-    private boolean incluidoEnElPlan;
-    private double sumaDeLasEvaluaciones;
-    private int totalDeLasEvaluaciones = 0;
+    private String director;
 
-    // setters
-    public void setNombre(String nombre){
-        this.nombre = nombre;
+    
+    public String getDirector(){
+        return director;
     }
 
-    public void setFechaDeLanzamiento(int fechaDeLanzamiento){
-        this.fechaDeLanzamiento = fechaDeLanzamiento;
-    }
-
-    public void setDuracionEnMinutos(int duracionEnMinutos) {
-        this.duracionEnMinutos = duracionEnMinutos;
-    }
-
-    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
-        this.incluidoEnElPlan = incluidoEnElPlan;
-    }
-
-    // getters
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public int getFechaDeLanzamiento() {
-        return fechaDeLanzamiento;
-    }
-
-    public int getDuracionEnMinutos() {
-        return duracionEnMinutos;
-    }
-
-    public boolean isIncluidoEnElPlan() {
-        return incluidoEnElPlan;
-    }
-
-    public int getTotalDeLasEvaluaciones(){
-        return totalDeLasEvaluaciones;
-    }
-
-    public void muestraFichaTecnica(){
-        System.out.println("El nombre de la película es: " + nombre);
-        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
-        System.out.println("Duración de la película: " + duracionEnMinutos);
-    }
-
-    public void evalua(double nota){
-        sumaDeLasEvaluaciones += nota;
-        totalDeLasEvaluaciones++;
-    }
-
-    public double calculaMedia(){
-        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    public void setDirector(String director){
+        this.director = director;
     }
 }
---
+++ src/com/aluracursos/screenmtach/modelos/Titulo.java
@@ -0,0 +1,64 @@
+package com.aluracursos.screenmtach.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaDeLanzamiento;
+    private int duracionEnMinutos;
+    private boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalDeLasEvaluaciones = 0;
+
+    // setters
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void setFechaDeLanzamiento(int fechaDeLanzamiento){
+        this.fechaDeLanzamiento = fechaDeLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    // getters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaDeLanzamiento() {
+        return fechaDeLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public int getTotalDeLasEvaluaciones(){
+        return totalDeLasEvaluaciones;
+    }
+
+    public void muestraFichaTecnica(){
+        System.out.println("El nombre de la película es: " + nombre);
+        System.out.println("Su fecha de lanzamiento es: " + fechaDeLanzamiento);
+        System.out.println("Duración de la película: " + duracionEnMinutos);
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalDeLasEvaluaciones++;
+    }
+
+    public double calculaMedia(){
+        return sumaDeLasEvaluaciones / totalDeLasEvaluaciones;
+    }
+}
