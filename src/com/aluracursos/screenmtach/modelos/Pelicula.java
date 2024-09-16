8d64df08b83814d0d0673aaeee5169285388d488
ruldiaz <rulhdiaz@gmail.com>
2024-09-16 11:54:22 -0600

    commit

--- src/com/aluracursos/screenmtach/modelos/Pelicula.java
+++ src/com/aluracursos/screenmtach/modelos/Pelicula.java
@@ -1,13 +1,14 @@
 package com.aluracursos.screenmtach.modelos;
 
-public class Pelicula {
+public class Pelicula extends  Titulo {
     private String director;
 
-    
+    // getters
     public String getDirector(){
         return director;
     }
 
+    // setters
     public void setDirector(String director){
         this.director = director;
     }
---
+++ src/com/aluracursos/screenmtach/modelos/Serie.java
@@ -0,0 +1,33 @@
+package com.aluracursos.screenmtach.modelos;
+
+public class Serie extends Titulo {
+    private int temporadas;
+    private int episodiosPorTemporada;
+    private int minutosPorEpisodio;
+
+    // getters
+    public int getTemporadas(){
+        return temporadas;
+    }
+
+    public int getEpisodiosPortemporada(){
+        return episodiosPorTemporada;
+    }
+
+    public int getMinutosPorEpisodio(){
+        return minutosPorEpisodio;
+    }
+
+    // setters
+    public void setTemporadas(int temporadas){
+        this.temporadas = temporadas;
+    }
+
+    public void setEpisodiosPorTemporada(int episodiosPorTemporada){
+        this.episodiosPorTemporada = episodiosPorTemporada;
+    }
+
+    public void setMinutosPorEpisodio(int minutosPorEpisodio){
+        this.minutosPorEpisodio = minutosPorEpisodio;
+    }
+}
